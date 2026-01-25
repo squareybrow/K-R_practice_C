cf9cb68d5aa6be2c3afa237915c312a72649a393
squareybrow <rishav5459@outlook.com>
2026-01-25 11:09:42 +0530

    feat: echo.c

---
+++ echo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
+  }
+  printf("\n");
+
+  while (--argc > 0) {
+    printf("%s%s", *++argv, (argc > 1) ? " " : "");
+  }
+  printf("\n");
+  return 0;
+}
